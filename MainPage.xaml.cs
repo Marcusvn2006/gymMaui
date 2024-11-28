ae7e4a1e7d87b03531fde10d2ecbcd4f98949336
Marcusvn2006 <zubuff@gmail.com>
2024-11-28 08:30:52 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using DevQuiz.Pages;
+
+namespace DevQuiz
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            SecureStorage.Default.RemoveAll();
+        }
+
+       
+    
+    }
+
+}
---
+++ Pages/Login.xaml.cs
@@ -0,0 +1,9 @@
+namespace DevQuiz.Pages;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+}
